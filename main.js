12261b023c4df25474ab129416497264f39e5606
Raiane Oliveira <raiane.oliveira404@gmail.com>
2023-03-09 16:00:49 -0300

    feat: API call with axios

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 import { API_KEY, BASE_URL, IMG_URL, language } from "./api.js";
+
+const btnFindMovie = document.querySelector(".find-movie-btn-app");
+btnFindMovie.addEventListener("click", findMovie);
+
+async function findMovie() {
+    // Generate random number
+    const LIMIT_OF_MOVIES = 20000;
+    const randomNumber = Math.floor(Math.random() * LIMIT_OF_MOVIES) + 1;
+
+    try {
+        const movieInformation = await axios
+            .get(`${BASE_URL}${randomNumber}?api_key=${API_KEY}`)
+            .then((response) => response.data);
+    } catch (e) {
+        console.log(e.message);
+    }
+}

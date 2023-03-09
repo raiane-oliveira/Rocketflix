b30c8812d57fdaf42823517bba7ecc0858b0e674
Raiane Oliveira <raiane.oliveira404@gmail.com>
2023-03-09 14:31:46 -0300

    :tada: Initial commit

---
+++ api.js
@@ -0,0 +1,4 @@
+export const API_KEY = "a1bdaf3d8d92b31bef526f35d5d91efd";
+export const BASE_URL = "https://api.themoviedb.org/3/movie/";
+export const IMG_URL = "https://image.tmdb.org/t/p/w500";
+export const language = "language=pt-BR";

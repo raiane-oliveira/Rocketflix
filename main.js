77860bbeb348ee8362c99604adb133b257979142
Raiane Oliveira <raiane.oliveira404@gmail.com>
2023-03-09 16:16:51 -0300

    feat: data movies on action

--- main.js
+++ main.js
@@ -8,10 +8,19 @@ async function findMovie() {
     const LIMIT_OF_MOVIES = 20000;
     const randomNumber = Math.floor(Math.random() * LIMIT_OF_MOVIES) + 1;
 
+    const containerMovies = document.querySelector(".container-movies-app");
+    containerMovies.style.display = "grid";
+
     try {
-        const movieInformation = await axios
-            .get(`${BASE_URL}${randomNumber}?api_key=${API_KEY}`)
+        const movie = await axios
+            .get(`${BASE_URL}${randomNumber}?api_key=${API_KEY}&language=pt-BR`)
             .then((response) => response.data);
+
+        containerMovies.innerHTML = `
+            <img class='poster-movie' src='${IMG_URL}/${movie.poster_path}' alt=${movie.title} Poster />
+            <h2 class='title-movie'>${movie.title}</h2>
+            <p>${movie.overview}</p>
+        `;
     } catch (e) {
         console.log(e.message);
     }
